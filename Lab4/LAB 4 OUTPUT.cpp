cdb60bdc96066a71d0cd611cead80348db39edc8
Dylan <Dylan.Brown@georgebrown.ca>
2021-02-17 12:58:14 -0500

    idek

--- Lab4/LAB 4 OUTPUT.cpp
+++ Lab4/LAB 4 OUTPUT.cpp
@@ -12,7 +12,7 @@ int main()
 	string haiku;
 	//Get Haiku
 	cout << "Please type out a haiku following the traditional method of 5-7-5 syllable structure!\n";
-	haiku = "The lord of the Rings, The battle for middle ear-, th. Is a bad name!v2";
+	haiku = "The lord of the Rings, The battle for middle ear-, th. Is a bad name!";
 	cout << haiku << "\nRecieved\n";
 
 	//Store the Poem
@@ -25,35 +25,6 @@ int main()
 	cout << "\nYour haiku has been saved to haiku.txt";
 	
 	
-	/*
-	/*
-#include <iostream>
-#include <fstream>
-#include <string>
-
-using namespace std;
-
-int main()
-{
 	
-	//Declare Variables
-	ifstream inputFile;
-	string haiku;
-
-	//Get Haiku
-	cout << "Usersubmitted haiku, topic is: Gaming\n";
-
-	// Open the poem
-	inputFile.open("haiku.txt");
-	getline(inputFile, haiku);
-	cout << "You have submitted the following haiku :" << haiku << endl;
-
-
-	//Close file
-	inputFile.close();
-
-	return 0;
-	
-	*/
 	return 0;
 }
